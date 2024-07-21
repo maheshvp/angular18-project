6aa57e05eb6163071916db645169563af6d7cde4
maheshvp <maheshvp.cs@gmail.com>
2024-07-21 22:11:15 +0530

    Implement task service

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 
 @Component({
@@ -12,22 +13,24 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  private tasksService = inject(TasksService);
   
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit(){
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDueDate
-    });
+    }, this.userId);
+    this.close.emit();
   }
 }
